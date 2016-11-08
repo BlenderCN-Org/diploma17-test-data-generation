0989bbffcaa55883e482abf015966d2e92fa48f9
fantasticisme <sanchousic@gmail.com>
2016-11-09 02:11:27 +0700

    sequence: sequence has been created

---
+++ sequence.py
@@ -0,0 +1,29 @@
+"""
+Copyright (c) 2016 Alexandr Menkin 
+Use of this source code is governed by an MIT-style license that can be 
+found in the LICENSE file at https://github.com/sanchousic/diploma17-test-data-generation/blob/master/LICENSE
+
+This file contains a set of functions for work with sequence.
+Sequence is an array of pairs.
+Pair is a tuple (action, parameters) created for action(parameters) execution
+Snapshot_function: function with type: function(parameter: int): void
+
+Information about actions can be found in the action.py file which is contained in repository at
+https://github.com/sanchousic/diploma17-test-data-generation/
+"""
+
+
+def handle(sequence, snapshot_function):
+    """ execute actions one by one with "making a snapshot" after every one
+
+    :param sequence: [pair_1 ... pair_n] where
+        pair_i is a (action, action_parameters)
+    :param snapshot_function: function(parameter: int): void which is called after every action's execution
+    """
+    index = 0
+    for pair in sequence:
+        action_function, parameters = pair
+        action_function(parameters)
+        snapshot_function(index)
+        index += 1
+    return
