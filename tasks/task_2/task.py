1a03a4e686775788edf3c40a57b1f7765bef6f7c
Alexandr Menkin <sanchousic@gmail.com>
2016-11-15 12:45:53 +0700

    task 2: pre-version

--- tasks/task_2/task.py
+++ tasks/task_2/task.py
@@ -15,103 +15,132 @@ import bpy
 import numpy as np
 import random
 import math
+import mathutils
+from blender_sequence import action as b_action
 from sequence import sequence
 from tasks import blender_nodes as b_nodes
-from tasks.task_2 import complex_generator
+from tasks.task_2 import complex_sequence
 from tasks.task_2 import generator as t_generator
 
 
 def go():
-    """ Task 2 """
+    """ Main function for task 2"""
+
+    # ellipsoid
     _object = bpy.data.objects['Sphere']
 
-    """ operations
 
-    operations:
-        1. lying - static:              no_motion
-        2. lying - forward motion:      y_forward_motion
-        3. lying - rotation:            z_rotation_motion
-        4. sitting up:                  sitting_up_lying_down
-        5. sitting - static:            no_motion
-        6. sitting - forward motion:    x_back_motion
-        7. sitting - rotation:          z_rotation_motion
-        8. lying down:                  sitting_up_lying_down
+    # generators
+    no_motion = t_generator.NoMotion(min_frames=20,
+                                     max_frames=40)
 
-    generators:
-        1. no_motion(min_frames, max_frames)
-        2. y_forward_motion(_object, min_length, max_length, min_speed, max_speed)
-        3. x_back_motion(_object, min_length, max_length, min_speed, max_speed)
-        4. z_rotation_motion(_object, min_angle, max_angle, min_speed, max_speed)
-        5. sitting_up_lying_down(_object, min_speed, max_speed)
-    """
-    operations = ((t_generator.no_motion,               (20, 40),                           1),
-                  (t_generator.y_forward_motion,        (_object, 2, 3, 0.1, 0.25),         2),
-                  (t_generator.z_rotation_motion,       (_object, 20, 40, 2, 3),            3),
-                  (t_generator.sitting_up_lying_down,   (_object, 2, 3),                    4),
-                  (t_generator.no_motion,               (20, 40),                           5),
-                  (t_generator.x_back_motion,           (_object, 1, 2.5, 0.05, 0.2),       6),
-                  (t_generator.z_rotation_motion,       (_object, 20, 40, 2, 4),            5),
-                  (t_generator.sitting_up_lying_down,   (_object, -4, -2),                  7))
+    lying_forward_motion = t_generator.DirectObjectMotionGenerator(_object=_object,
+                                                                   local_direction=mathutils.Vector((0, 1, 0)),
+                                                                   distance=1.5,
+                                                                   min_step=0.1,
+                                                                   max_step=0.3)
 
-    probabilities = np.array([[0.4,  0.2,  0.2, 0.2, 0,    0,    0,   0  ],
-                              [0.2,  0.3,  0.3, 0.2, 0,    0,    0,   0  ],
-                              [0.2,	 0.2,  0.4, 0.2, 0,    0,    0,   0  ],
-                              [0,    0,    0,   0,   0.45, 0.45, 0.1, 0  ],
-                              [0,    0,    0,   0,   0.2,  0.4,  0.1, 0.3],
-                              [0,    0,    0,   0,   0.2,  0.1,  0.3, 0.4],
-                              [0,    0,    0,   0,   0.1,  0.2,  0.2, 0.5],
-                              [0.45, 0.45, 0.1, 0,   0,    0,    0,   0  ]],
+    sitting_forward_motion = t_generator.DirectObjectMotionGenerator(_object=_object,
+                                                                     local_direction=mathutils.Vector((0, 1, 0)),
+                                                                     distance=1,
+                                                                     min_step=0.1,
+                                                                     max_step=0.2)
+
+    sitting_z_rotation = t_generator.ZRotationMotion(_object=_object,
+                                                     min_angle=30,
+                                                     max_angle=60,
+                                                     min_d_angle=2,
+                                                     max_d_angle=3)
+
+    lying_z_rotation = t_generator.ZRotationMotion(_object=_object,
+                                                   min_angle=20,
+                                                   max_angle=40,
+                                                   min_d_angle=1.5,
+                                                   max_d_angle=2)
+
+    sitting_up = t_generator.ZeroZSittingXRotation(_object=_object,
+                                                   limit_angle=90,
+                                                   is_increase=True,
+                                                   min_d_angle=2,
+                                                   max_d_angle=3)
+
+    lying_down = t_generator.ZeroZSittingXRotation(_object=_object,
+                                                   limit_angle=0,
+                                                   is_increase=False,
+                                                   min_d_angle=-3,
+                                                   max_d_angle=-2)
+
+
+    # ProbabilisticComplexSequence
+    generators = [no_motion,
+                  lying_forward_motion,
+                  lying_z_rotation,
+                  sitting_up,
+                  no_motion,
+                  sitting_forward_motion,
+                  sitting_z_rotation,
+                  lying_down]
+
+    logs = [1, 2, 3, 4, 5, 6, 5, 7]
+
+    probabilities = np.array([[0.4, 0.2, 0.2, 0.2, 0, 0, 0, 0],
+                              [0.2, 0.3, 0.3, 0.2, 0, 0, 0, 0],
+                              [0.2, 0.2, 0.4, 0.2, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0.45, 0.45, 0.1, 0],
+                              [0, 0, 0, 0, 0.2, 0.4, 0.1, 0.3],
+                              [0, 0, 0, 0, 0.2, 0.1, 0.3, 0.4],
+                              [0, 0, 0, 0, 0.1, 0.2, 0.2, 0.5],
+                              [0.45, 0.45, 0.1, 0, 0, 0, 0, 0]],
                              dtype=np.float)
-    journal = []
-    transforms = []
-    n = 1
 
     def init():
-
         # scale
-        scale_x = random.uniform(0.2, 0.3)
-        scale_y = random.uniform(0.5, 0.7)
-        scale_z = random.uniform(scale_x - 0.05, scale_x + 0.05)
+        scale_x = random.uniform(0.1, 0.15)
+        scale_y = random.uniform(0.25, 0.35)
+        scale_z = random.uniform(scale_x - 0.025, scale_x + 0.025)
         location_x = random.uniform(-1, 1)
         location_y = random.uniform(-1, 1)
         rotation_y = math.radians(0.0)
         rotation_z = random.uniform(math.radians(0), math.radians(360))
-
-        random_k = random.uniform(0.0, 1.0)
         location_z = scale_z
         rotation_x = math.radians(0.0)
         state = random.choice([0, 1, 2, 3])
-        if random_k > 0.75:
+        if random.choice([True, False]):
             location_z = scale_y
             rotation_x = math.radians(90.0)
             state = random.choice([4, 5, 6, 7])
+        b_action.set_location((_object, location_x, location_y, location_z))
+        b_action.set_rotation((_object, rotation_x, rotation_y, rotation_z))
+        _object.scale = (scale_x, scale_y, scale_z)
+        return state
 
-        return state, ((location_x, location_y, location_z),
-                       (rotation_x, rotation_y, rotation_z),
-                       (scale_x, scale_y, scale_z))
+    # prepare for sequence's host
+    journal = []
+    n_modulation = 2
+    performances_per_modulation = 10
 
-    # main part
-    for i in range(n):
-        init_state, transform = init()
-        transforms.append(transform)
-        performances = 10
-        parameters = (init_state, operations, probabilities, performances)
-        journal.append(complex_generator.probabilistic_generator(parameters))
+    def check():
+        x = _object.location.x
+        y = _object.location.y
+        return (-8.5 < x < 8.5)  and (-6.2 < y < 6.2)
 
     # init nodes
     b_nodes.init_for_z_depth_with_noise(count_z_depth_parameters(15, 3, 0))
 
-    for i in range(n):
-        def snapshot_function(index):
-            generate_image(i + 1, index)
+    # main part
+    for i in range(n_modulation):
+        init_state = init()
+        pcs = complex_sequence.ProbabilisticComplexSequence(init_state, generators, logs, probabilities, performances_per_modulation)
+
+        def snapshot(index):
+            #generate_image(i, index)
             return
-        _sequence, log = journal[i]
-        _object.location = transforms[i][0]
-        _object.rotation_euler = transforms[i][1]
-        _object.scale = transforms[i][2]
-        print(log)
-        sequence.handle(_sequence, snapshot_function)
-        # TODO save log
+
+        sequence.handle(pcs, check, snapshot)
+        journal.append(pcs.logs_journal)
+        print("")
+        print(journal[i])
+
 
     return
 
